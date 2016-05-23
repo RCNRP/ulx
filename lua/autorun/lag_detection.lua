1f81ef57261fae41aa06ce22e6f132b629ded6ba
zacklogan <bigred9921@mchsi.com>
2016-05-23 11:05:55 -0500

    Created
    Created lag detection system with ULX commands to turn off / on (Later to change the TPS it'll go off on)

---
+++ lua/autorun/lag_detection.lua
@@ -0,0 +1,75 @@
+if SERVER then
+lagdetect = "on"
+print("Lag detection starting...")
+EntitysLagg = 0
+EntityslastOS = 0
+EntitysTimer = 1
+Entitystbl = {}
+EntitysCanFreeze = {
+    "prop_physics"
+}
+EntitysMinThinks = 40
+timer.Simple(5,function() -- to ignore the thinks at startup (it always lags then:P)
+    hook.Add("Think","EntitysLagg",function() -- add the hook
+        EntitysTimer = EntitysTimer + 1 -- add the checker
+        if os.time() != EntityslastOS then -- if theres a second passed
+            EntitysOScheck() -- do the check function
+            EntityslastOS = os.time() -- update the last time!
+        end
+    end)
+end)
+
+function EntitysOScheck() 
+    if lagdetect == "on" then
+    if EntitysTimer <= EntitysMinThinks then -- if the TPS is lower then the minimal requres thinks per second!
+        EntitysLagg = EntitysLagg + 1 -- add lagg check
+    elseif EntitysLagg > 0 then -- if not, remove one, not set it to 0, if its fixed for just one check, then we can restart!
+        EntitysLagg = EntitysLagg - 1 -- remove some lagg
+    end
+    
+    if EntitysLagg >= 10 then -- HUSTION WE ARE LAGGING!
+        local seeasayAccess = "ulx seeasay"
+        local players = player.GetAll()
+        print("Server: Lag detection has been tripped. TPS: " .. EntitysTimer)
+        for i=#players, 1, -1 do
+            local t = players[ i ]
+            if ULib.ucl.query( t, seeasayAccess ) then
+                t:PrintColor(Color( 255, 0, 0 ), "[Admin Notification]:", Color( 255, 255, 255 ), " Server appears to be lagging, lets try fixing that. | TPS: " .. EntitysTimer )
+            end
+        end
+        RunConsoleCommand("ulx", "lag")
+        EntitysLagg = 0 -- reset the lag
+    end 
+    
+    EntitysTimer = 0 -- reset the TPS
+    end
+end
+
+util.AddNetworkString( "PrintColor" )
+
+local Meta = FindMetaTable( "Player" )
+
+function Meta:PrintColor( ... )
+
+	net.Start( "PrintColor" )
+		net.WriteTable( { ... } )
+	net.Send( self )
+end
+
+end
+
+function ulx.lagoff( calling_ply )
+    lagdetect = "off"
+    ulx.fancyLogAdmin( calling_ply, "#A turned off the lag detection system" )
+end
+local lagoff = ulx.command( "Logan's Custom Things", "ulx lagoff", ulx.lagoff, "!lagoff" )
+lagoff:defaultAccess( ULib.ACCESS_ADMIN )
+lagoff:help( "Turns off lag dectection" )
+
+function ulx.lagon( calling_ply )
+    lagdetect = "on"
+    ulx.fancyLogAdmin( calling_ply, "#A turned on the lag detection system" )
+end
+local lagon = ulx.command( "Logan's Custom Things", "ulx lagon", ulx.lagon, "!lagon" )
+lagon:defaultAccess( ULib.ACCESS_ADMIN )
+lagon:help( "Turns on lag detection." )
