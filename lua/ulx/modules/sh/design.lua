b26cd10a93b4b5edea14d4ff8b3f27ddd0547472
zacklogan <bigred9921@mchsi.com>
2016-05-27 09:25:09 -0500

    Things to change in the design

---
+++ lua/ulx/modules/sh/design.lua
@@ -0,0 +1,10 @@
+VGUI Frame
+-----------------
+Frame.Paint = function( self, w, h ) -- 'function Frame:Paint( w, h )' works too
+	draw.RoundedBox( 0, 0, 0, w, h, Color( 64, 64, 64, 255 ) ) -- Draw a red box instead of the frame
+end
+
+
+Remove buttons color.
+
+Change DNumSlider to a DNumberWang
